383c03e4f12f95bb44437ade17b7749e0cb26afe
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-03 17:59:13 -0800

    done

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *newnode;
 
 	if (parent == NULL)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 
 	newnode = binary_tree_node(parent, value);
 
