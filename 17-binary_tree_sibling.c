6afa99d5a3fbc8e3502744473828cf1cd050f954
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-04 08:03:46 -0800

    check

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node in a binary tree
+ * @node: Pointer to the node to find the sibling of
+ *
+ * Return: Pointer to the sibling node, or NULL if no sibling or node is NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!(node) || !(node->parent))
+		return (NULL);
+
+	if (node->parent->left != node && node->parent->left != NULL)
+	{
+		return (node->parent->left);
+	}
+	else if (node->parent->right != node && node->parent->right != NULL)
+	{
+		return (node->parent->right);
+	}
+
+	return (NULL);
+}
