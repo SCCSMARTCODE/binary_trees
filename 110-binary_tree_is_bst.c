42028042f36ddb2af3992844d2c2c8cd89fe6b0b
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-04 12:48:52 -0800

    done

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -25,13 +25,12 @@ int binary_tree_is_bst(const binary_tree_t *tree)
  */
 int btib_helper(const binary_tree_t *tree, int min, int max)
 {
-        if (!tree)
-                return (1);
+	if (!tree)
+		return (1);
 
-        if (tree->n < min || tree->n > max)
-                return (0);
+	if (tree->n < min || tree->n > max)
+	return (0);
 
-        return (btib_helper(tree->left, min, tree->n - 1) &&
-                btib_helper(tree->right, tree->n + 1, max));
-        /* -1 and +1 stem from "There must be no duplicate values" req */
+	return (btib_helper(tree->left, min, tree->n - 1) &&
+		btib_helper(tree->right, tree->n + 1, max));
 }
