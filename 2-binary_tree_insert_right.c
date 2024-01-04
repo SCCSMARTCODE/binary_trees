2826dbe210ba39922b6d2eafb2ece3159aa8ea65
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-03 17:53:58 -0800

    tree

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to be stored in the new node
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL || parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->right == NULL)
+	{
+		parent->right = newnode;
+	}
+	else
+	{
+		newnode->right = parent->right;
+		parent->right->parent = newnode;
+		parent->right = newnode;
+	}
+
+	return (newnode);
+}
