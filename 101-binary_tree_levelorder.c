1c744aad2afc638bd8ab87f4e1af42ded6fb05f6
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-04 08:51:53 -0800

    check

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * btlo_helper - goes through a binary tree using post-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ * @level: the level of the tree to call func upon
+ */
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
+	}
+}
+
+/**
+ * binary_tree_levelorder - traverst a binary tree using level-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t level, maxlevel;
+
+	if (!tree || !func)
+		return;
+
+	maxlevel = binary_tree_height(tree) + 1;
+
+	for (level = 1; level <= maxlevel; level++)
+		btlo_helper(tree, func, level);
+}
