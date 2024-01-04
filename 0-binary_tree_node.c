1e197b012f29331675d0babdf3c5a89a63390020
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-03 17:32:48 -0800

    edited

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: Pointer to the newly created node
+ *         NULL on failure
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -12,6 +20,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	newnode->parent = parent;
 	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
 	
 	return (newnode);
 }
