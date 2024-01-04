c3c562be4a5dcff370b240d6b2526548a8e34c05
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-03 17:37:22 -0800

    edited

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,16 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_insert_left - Inserts a node as the left child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to be stored in the new node
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
-		
+
 	newnode = binary_tree_node(parent, value);
 
 	if (newnode == NULL || parent == NULL)
